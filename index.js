13d56b1d43521079e0017a84175d0ba776182b75
Mayank Jariya <142983479+mayankjariya8808@users.noreply.github.com>
2025-03-07 13:12:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const fs = require("fs");
 const puppeteer = require("puppeteer");
 
 const app = express();
-const PORT = process.env.PORT || 5000 ;
+const PORT = process.env.PORT || 5500 ;
 
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/tripDB";
